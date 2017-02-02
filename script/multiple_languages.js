0859307be9fbb4630f0835dd3fa02941590ad1ee
Diana <Diana>
2017-02-02 21:12:18 +0200

    add eng and russ languages

--- script/multiple_languages.js
+++ script/multiple_languages.js
@@ -1,15 +1,288 @@
 var arrLang = {
-    'eng': {
-        'about_company': 'About Company',
-        'company_news': 'Company News'
+        'ukr': {
+            
+        /*header*/
+        'enter_in':'Вхід',
+        'and':'і',
+        'registr':'Реєстрація',
+        'help_center':'Центр підтримки',
+        'clients':'клієнтів',
+            
+        /*navigation*/
+        /*about*/
+        'about_company': 'Про компанію',
+        'company_news': 'Новини компанії',
+        'company_today': '"Нова пошта" сьогодні',
+        'soc_respon': 'Соціальна відповідальність',
+        'sup_board': 'Наглядова рада',
+        'working_at': 'Робота в компанії',
+        'become_a_rep': 'Стати партнером',
+        'supplier': 'Постачальникам',
+        'press_center': 'Прес-центр',
+        'fin_services': 'Фінансові послуги',
+        'recommends': '"Нова пошта" рекомендує',
+        
+        /*for private customers*/
+        'for_priv_cust': 'Приватним клієнтам',
+        'shipping': 'Відправити',
+        'receive': 'Отримати',
+        'payment': 'Оплата',
+        'add_serv': 'Додаткові послуги',
+        'docum': 'Документи',
+        'rates': 'Тарифи',
+        
+        /*for bussines customers*/
+        'for_buss_cust': 'Бізнес клієнтам',
+        'for_online_stores': 'Інтернет-магазинам',
+        'for_corp_cust': 'Корпоративним клієнтам',
+        'fullfilment': 'Фулфілмент',
+        'open_an_account': 'Укласти договір',
+        'programm_of_cooper': 'Програма співпраці з клієнтами',
+        
+        /*international delivery*/
+        'inter_deliv': 'Міжнародна доставка',
+        'about_inter_deliv': 'Про Міжнародну доставку',
+        'how_it_work': 'Доставка по всьому світу',
+        'ship_moldova_georgia': 'Доставка у молдову у Грузію',
+        'del_from_china': 'Доставка з Китаю',
+        'np_ship': 'NP Shopping',
+        'e_commerce': 'TE-commerce імпорт',
+        
+        /*depots*/
+        'depots': 'Відділення',
+        'near_depots': 'Найближче відділення',
+        'depots_buss_hours': 'Графік роботи відділень',
+        'news_branches': 'Інформація про роботу відділень',
+        
+        /*loyal program*/
+        'loyal_program': 'Програма лояльності',
+        'about_program': 'Про програму',
+        'your_remun': 'Твоя винагорода',
+        'pers_account': 'Особистий кабінет',
+        
+        /*contacts*/
+        'contacts': 'Контакти',
+        'feedback': 'Зворотній зв\'язок',
+            
+        /*track*/
+        'track': 'Відстежити',
+        'in_numb': 'Номер накладної',
+        'enter_in_numb': 'Введіть номер накладної',
+        'del_price': 'Вартість доставки',
+        'term':'Терміни доставки',
+        'depot':'Найближче відділення',
+        'hours':'Графік роботи відділень',
+        'courier':'Виклик кур\'єра',
+        
+            
+        /*mobile app*/
+        'download': 'Завантажити',
+        'mob_app': 'мобільний додаток',
+        'for' : 'для:',
+            
+        'all_right': 'AВсі права захищені. Використання матеріалів даного сайту можливе тільки з посиланням на джерело.',
+        'future_nova_poshta': 'Доставка товарів вантажоперевезення «Нова пошта – доставка майбутнього»',
+        'calling': 'Дзвінки по всій території України безкоштовні',
+            
+        /*footer*/
+        'fnews':'Новини',
+        'quest':'Питання-Відповідь',
+        'portal':'Портал разработчика API 2.0'
     },
     
-    'ukr': {
-        'about_company': 'Про компанію',
-        'company_news': 'Новони Компанії'
-    }
+    'eng': {
+        /*header*/
+        'enter_in':'Log on',
+        'and':'and',
+        'registr':'Registration',
+        'help_center':'Customer support',
+        'clients':'center',
+        
+        /*navigation*/
+        /*about*/
+        'about_company': 'About',
+        'company_news': 'Company news',
+        'company_today': 'Nova Poshta today',
+        'soc_respon': 'Corporate social responsibility',
+        'sup_board': 'Supervisory board',
+        'working_at': 'Working At Nova Poshta',
+        'become_a_rep': 'Become a Representative',
+        'supplier': 'For Suppliers',
+        'press_center': 'Press Center',
+        'fin_services': 'Financial services',
+        'recommends': 'The Nova Poshta recommends',
+        
+        /*for private customers*/
+        'for_priv_cust': 'For Private Customers',
+        'shipping': 'Shipping',
+        'receive': 'Receive',
+        'payment': 'Payment',
+        'add_serv': 'Additional Services',
+        'docum': 'Documents',
+        'rates': 'Rates',
+        
+        /*for bussines customers*/
+        'for_buss_cust': 'For Bussines Customers',
+        'for_online_stores': 'For Online Stores',
+        'for_corp_cust': 'For Corporate Customers',
+        'fullfilment': 'Fullfilment',
+        'open_an_account': 'Open an account',
+        'programm_of_cooper': 'The programm of cooperation with partners',
+        
+        /*international delivery*/
+        'inter_deliv': 'International delivery',
+        'about_inter_deliv': 'About International delivery',
+        'how_it_work': 'How does it work',
+        'ship_moldova_georgia': 'Shipping in Moldova and eogia',
+        'del_from_china': 'Delivery from China',
+        'np_ship': 'NP Shopping',
+        'e_commerce': 'TE-commerce імпорт',
+        
+        /*depots*/
+        'depots': 'Depots',
+        'near_depots': 'Nearest Depot',
+        'depots_buss_hours': 'Depots Bussiness Hours',
+        'news_branches': 'News branches',
+        
+        /*loyal program*/
+        'loyal_program': 'Програма лояльності',
+        'about_program': 'About Programm',
+        'your_remun': 'Your Remuneration',
+        'pers_account': 'Personal Account',
+        
+        /*contacts*/
+        'contacts': 'Contacts',
+        'feedback': 'Feedback',
+        
+        /*track*/
+        'track':'Track',
+        'in_numb':'Invoice number',
+        'enter_in_numb':'Enter the invoice number',
+        'del_price': 'Delivery Price',
+        'term': 'Delivery Term',
+        'depot': 'Nearest Depot',
+        'hours':'Depots Bussiness Hours',
+        'courier': 'Call a Courier',
+        
+        /*mobile app*/
+        'download': 'Download',
+        'mob_app': 'the mobile application',
+        'for' : 'for:',
+        
+        /*rights*/
+        'all_right': 'All rights reserved. Use of this site is permitted only with reference to the source.',
+         'future_nova_poshta': '«Nova Poshta – delivery of the future»',
+        'calling': 'Calls in Ukraine are free',
+        
+        /*footer*/
+        'fnews':'News',
+        'quest':'FAQ',
+        'portal':'Портал разработчика API 2.0'
+    },
     
-}
+        'rus': {
+        /*header*/
+        'enter_in':'Вход',
+        'and':'и',
+        'registr':'Регистрация',
+        'help_center':'Центр поддержки',
+        'clients':'клиентов',
+            
+        /*navigation*/
+        /*about*/
+        'about_company': 'О компании',
+        'company_news': 'Новости компании',
+        'company_today': '"Нова пошта" сегодня',
+        'soc_respon': 'Социальная ответственность',
+        'sup_board': 'Наблюдательный Совет',
+        'working_at': 'Работа в компании',
+        'become_a_rep': 'Стать партнером',
+        'supplier': 'Поставщикам',
+        'press_center': 'Пресс-центр',
+        'fin_services': 'Финансовые услуги',
+        'recommends': '"Нова пошта" рекомендует',
+        
+        /*for private customers*/
+        'for_priv_cust': 'Частным клиентам',
+        'shipping': 'Отправить',
+        'receive': 'Получить',
+        'payment': 'Оплата',
+        'add_serv': 'Дополнительные услуги',
+        'docum': 'Документы',
+        'rates': 'Тарифы',
+        
+        /*for bussines customers*/
+        'for_buss_cust': 'Бизнес-кліентам',
+        'for_online_stores': 'Интернет-магазинам',
+        'for_corp_cust': 'Корпоративным клиентам',
+        'fullfilment': 'Фулфилмент',
+        'open_an_account': 'Заключить договор',
+        'programm_of_cooper': 'Програма сотрудничества с партнерами',
+        
+        /*international delivery*/
+        'inter_deliv': 'Межнародная доставка',
+        'about_inter_deliv': 'О міжнародну доставке',
+        'how_it_work': 'Доставка по всему миру',
+        'ship_moldova_georgia': 'Доставка в Молдову и Грузію',
+        'del_from_china': 'Доставка из Китая',
+        'np_ship': 'NP Shopping',
+        'e_commerce': 'TE-commerce импорт',
+        
+        /*depots*/
+        'depots': 'Отделение',
+        'near_depots': 'Ближайшее отделение',
+        'depots_buss_hours': 'График работы отделений',
+        'news_branches': 'нформация о работе отделений',
+        
+        /*loyal program*/
+        'loyal_program': 'Программа лояльности',
+        'about_program': 'О программе',
+        'your_remun': 'Твое вознаграждение',
+        'pers_account': 'Личный кабнет',
+        
+        /*contacts*/
+        'contacts': 'Контакты',
+        'feedback': 'Обратная связь',
+            
+        /*track*/
+        'track':'Отследить',
+        'in_numb': 'Номер накладной',
+        'enter_in_numb':'Введите номер накладной',
+        'del_price':'Стоимость доставки',
+        'term':'Сроки доставки',
+        'depot':'Ближайшее отделение',
+        'hours':'График работы отделений',
+        'courier':'Вызов курьера',
+            
+        /*mobile app*/
+        'download': 'Скачать',
+        'mob_app': 'мобильное приложение',
+        'for' : 'для:',
+            
+         /*news
+        'wether': 'Скачать',
+        'viber': 'мобильное приложение',
+        'mod_1C' : 'для:', 
+        'wether': 'Скачать',
+        'viber': 'мобильное приложение',
+        'mod_1C' : 'для:', 
+        'wether': 'Скачать',
+        'viber': 'мобильное приложение',
+        'mod_1C' : 'для:', */
+            
+        /*rights*/
+        'all_right': 'Все права защищены. Использование материалов данного сайта только с указанием ссылки на источник.',
+        'future_nova_poshta': '«Нова пошта – доставка будущего»',
+        'calling': 'Звонки по всей территории Украины бесплатные',
+            
+        /*footer*/
+        'fnews':'Новости',
+        'quest':'Вопрос-Ответ',
+        'portal':'Портал разработчика API 2.0'
+    }
+
+    
+};
 
 
 $(function(){
@@ -17,7 +290,7 @@ $(function(){
         var lang = $(this).attr('id');
         
         $('.lang').each(function(index,element){
-            $('.this').text(arrLang[lang][$(this).attr('key')]);
+            $(this).text(arrLang[lang][$(this).attr('key')]);
         });
     });
 });
