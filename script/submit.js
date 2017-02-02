45bfdca744052632f3494c0be9a8d0d1b3ed1db9
Diana <Diana>
2017-02-02 17:30:33 +0200

    add background, fixed css

--- script/submit.js
+++ script/submit.js
@@ -3,7 +3,14 @@ function validation () {
   var trackNumb = $("[id='item_number']").val();
   var numbPattern =  /^[0-9]/;  
   if (!numbPattern.test(trackNumb)){
-   alert('Tracker number must contain only numbers');
+   alert('Waybill number must contain only numbers!');
+   return false;   
+  }
+    
+  var trackNumb = $("[id='item_number']").val();
+  var numbPattern =  /^[0-9]{14}/;  
+  if (!numbPattern.test(trackNumb)){
+   alert('Waybill number must contain 14 numbers!');
    return false;   
   }
 }
